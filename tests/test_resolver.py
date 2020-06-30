df1e46f87f48d3ffa3969f49d656a02079ef8d62
nyanchan <personal@nyanchangames.com>
2020-06-30 22:00:06 +0900

    Place test data into tests/data

--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -8,12 +8,13 @@ import resolver
 class TestResolver(unittest.TestCase):
 	def test_resolveLocal(self):
 		r=resolver.Resolver()
-		resolved=r.resolve("tests/dl.md")
-		self.assertEqual(resolved, "tests/dl.md")
+		fname="tests/data/dl.md"
+		resolved=r.resolve(fname)
+		self.assertEqual(resolved, fname)
 
 	def testHttps(self):
 		fname="dl.md"
-		remote="https://github.com/actlaboratory/diff_archiver/raw/master/tests/dl.md"
+		remote="https://github.com/actlaboratory/diff_archiver/raw/master/tests/data/dl.md"
 		if os.path.exists(fname): os.remove(fname)
 		r=resolver.Resolver()
 		resolved=r.resolve(remote)
