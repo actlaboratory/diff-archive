fb16e5f31c1dfb0ae5ba0cf6e923dea54cd48e3a
nyanchan <personal@nyanchangames.com>
2020-07-01 20:34:06 +0900

    Add __init__ and setup.py

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from .diff_archiver import *
+
+__copyright__    = 'Copyright (C) 2020 Yukio Nozawa'
+__version__      = '1.0.0'
+__license__      = 'MIT'
+__author__       = 'Yukio Nozawa'
+__author_email__ = 'personal@nyanchangames.com'
+__url__          = 'https://github.com/actlaboratory/diff_archiver'
+
+__all__ = ['diff_archiver']
